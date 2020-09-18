662f5d7baa85cac032a13667d8c87b4926701fe1
wlnsss <1052290676@qq.com>
2020-09-18 08:40:42 +0800

    459、查找子串

--- src/lettercode/RepeatedSubstringPattern_459.java
+++ src/lettercode/RepeatedSubstringPattern_459.java
@@ -1,29 +1,31 @@
 package lettercode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RepeatedSubstringPattern_459 {
 	public boolean repeatedSubstringPattern(String s) {
         int n = s.length();
-        
-        for (int i = 1; i * 2 <= n; ++i) {
-            if (n % i == 0) {
-                boolean match = true;
-                for (int j = i; j < n; ++j) {
-                    if (s.charAt(j) != s.charAt(j - i)) {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match) {
-                    return true;
-                }
-            }
+        List<Integer> maybeList = new ArrayList<Integer>();
+        int halfN = n / 2;
+        //这里是遍历有可能的子串加入List,主要是看能否被整除，例如有15个字符的字符串则字串必定不可能是2，4，6，7
+        for(int i = 1;i <= halfN;i++) {
+        	if(!(((double)n / i) != (n / i))) {
+        		maybeList.add(i);
+        	}
+        }
+        for(Integer index : maybeList) {
+        	String str = s;
+        	str = str.replace(s.substring(0,index), "");
+        	if(str.isEmpty())
+        		return true;
         }
         return false;
     }
 	
 	public static void main(String[] args) {
 		RepeatedSubstringPattern_459 r = new RepeatedSubstringPattern_459();
-		r.repeatedSubstringPattern("abab");
+		r.repeatedSubstringPattern("ababa");
 	}
 
 }
