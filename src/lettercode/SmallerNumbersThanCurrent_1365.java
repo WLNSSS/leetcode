3b702e7f3f75106bd782ac8b3e44822b18b7ccf0
wlnsss <w1052290676@qq.com>
2020-10-26 23:10:51 +0800

    1365

---
+++ src/lettercode/SmallerNumbersThanCurrent_1365.java
@@ -0,0 +1,20 @@
+package lettercode;
+
+import java.util.Arrays;
+
+public class SmallerNumbersThanCurrent_1365 {
+	public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[i]) {
+                    cnt++;
+                }
+            }
+            ret[i] = cnt;
+        }
+        return ret;
+    }
+}
