ed6d911d9847c33432783e211114012759246665
wlnsss <1052290676@qq.com>
2020-12-30 10:19:55 +0800

    1046

---
+++ src/lettercode/LastStoneWeight_1046.java
@@ -0,0 +1,21 @@
+package lettercode;
+
+import java.util.PriorityQueue;
+
+public class LastStoneWeight_1046 {
+	public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            if (a > b) {
+                pq.offer(a - b);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
