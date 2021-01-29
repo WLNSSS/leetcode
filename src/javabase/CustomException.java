43986d1fba884f29d7409284fab05954568565b3
wlnsss <1052290676@qq.com>
2021-01-29 09:55:02 +0800

    test

---
+++ src/javabase/CustomException.java
@@ -0,0 +1,27 @@
+package javabase;
+
+public class CustomException extends Exception{
+	//无参构造方法
+		public CustomException(){
+			
+			super();
+		}
+		
+		//有参的构造方法
+		public CustomException(String message){
+			super(message);
+			
+		}
+		
+		// 用指定的详细信息和原因构造一个新的异常
+		public CustomException(String message, Throwable cause){
+			
+			super(message,cause);
+		}
+		
+		//用指定原因构造一个新的异常
+		 public CustomException(Throwable cause) {
+			 
+			 super(cause);
+		 }
+}
--- src/javabase/Test.java
+++ src/javabase/Test.java
@@ -7,40 +7,36 @@ import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 
 public class Test {
-	 private int age;
-	    private String name;
-	    private int testint;
-	 
-	    public Test(int age) {
-	        this.age = age;
-	    }
-	 
-	    public Test(int age, String name) {
-	        this.age = age;
-	        this.name = name;
-	    }
-	 
-	    private Test(String name) {
-	        this.name = name;
-	    }
-	 
-	    public Test() {
-	    }
-	    public static void main(String[] args) {
-	        try (//创建一个ObjectOutputStream输出流
-	             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("object.txt"))) {
-	            //将对象序列化到文件s
-	            Person person = new Person("9龙", 23);
-	            oos.writeObject(person);
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	        try (//创建一个ObjectInputStream输入流
-	                ObjectInputStream ois = new ObjectInputStream(new FileInputStream("person.txt"))) {
-	               Person brady = (Person) ois.readObject();
-	               System.out.println(brady);
-	           } catch (Exception e) {
-	               e.printStackTrace();
-	           }
-	    }
+public void test2() {
+		
+			
+			try {
+				test1();
+			} catch (CustomException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+	
+	}
+	
+	public void test1() throws CustomException{
+		
+		throw new CustomException("我喝酒了");
+	}
+// main方法
+	public static void main(String[] args) {
+		
+		Test  object =  new  Test();
+		
+		//try{
+			
+			object.test2();
+			
+		//}catch(Exception e){
+			
+		//e.printStackTrace();
+			
+		//}
+
+	}
 }
